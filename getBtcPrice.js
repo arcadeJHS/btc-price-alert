855c6444909b6279c3d2032be7e8d870f43e1eda
Matteo Piazza <matteo.piazza@exmachina.ch>
2024-01-03 16:34:47 +0100

    Update getBtcPrice.js

--- getBtcPrice.js
+++ getBtcPrice.js
@@ -1,13 +1,6 @@
 const btcRatesApiUrl = "https://api.coinbase.com/v2/exchange-rates?currency=BTC";
-let price = 0;
 
 fetch(btcRatesApiUrl).then(response => response.json()).then(data => {
-    const btcPrice = data.data.rates.CHF;
-
-    // console.log(btcPrice);
-
-    return btcPrice;
-}).then(price => {
-    price = parseFloat(price);
-    return price;
+    const price = data.data.rates.CHF;
+    console.log(parseFloat(price));
 });
