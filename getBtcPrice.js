7eef14eb7ddcd87d0d4d3d7bafd0517268dde1ab
Matteo Piazza <matteo.piazza@exmachina.ch>
2024-01-03 15:09:42 +0100

    Update getBtcPrice.js

--- getBtcPrice.js
+++ getBtcPrice.js
@@ -1,13 +1,17 @@
-const btcRatesApiUrl = "https://api.coinbase.com/v2/exchange-rates?currency=BTC";
+const requestPrice = async () => {
+    const btcRatesApiUrl = "https://api.coinbase.com/v2/exchange-rates?currency=BTC";
 
-const rates = await request.get(btcRatesApiUrl);
+    const rates = await fetch.get(btcRatesApiUrl);
 
-const ratesJson = await rates.json();
+    const ratesJson = await rates.json();
 
-const btcPrice = parseFloat(ratesJson.data.rates.CHF);
+    const btcPrice = parseFloat(ratesJson.data.rates.CHF);
 
-const sendAlert = (btcPrice > 39000 || btcPrice < 36000);
+    const sendAlert = (btcPrice > 39000 || btcPrice < 36000);
 
-echo `BTC Price:${btcPrice}`;
+    echo `BTC Price:${btcPrice}`;
 
-return sendAlert;
+    return sendAlert;
+};
+
+return requestPrice().then((sendAlert) => sendAlert);
