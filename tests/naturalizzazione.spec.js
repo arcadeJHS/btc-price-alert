cf95483ece8a45da5f89ad134bba9de5a9d1b9ca
Matteo Piazza <matteo.piazza@exmachina.ch>
2024-01-03 13:29:27 +0100

    Fixed test

--- tests/naturalizzazione.spec.js
+++ tests/naturalizzazione.spec.js
@@ -1,10 +1,15 @@
 import { test, expect } from '@playwright/test';
 
-const url = 'https://www.ifc.ti.ch/corsi/tutti-i-corsi/dettaglio/2324asoc0140001_corsi-di-formazione-alla-cittadinanza';
+test('Check BTC price', async ({ request }) => {
+  const btcRatesApiUrl = "https://api.coinbase.com/v2/exchange-rates?currency=BTC";
 
-test('Le iscrizioni al corso di naturalizzazione sono aperte?', async ({ page }) => {
-  await page.goto(url);
-  const btnIscriviti = await page.getByRole('button', { name: /iscriviti/i });
-  // await expect(btnIscriviti).toHaveClass(/disabled/);
-  await expect(btnIscriviti).not.toBeVisible();
+  const rates = await request.get(btcRatesApiUrl);
+
+  expect(rates.ok()).toBeTruthy();
+
+  const ratesJson = await rates.json();
+
+  const btcPrice = ratesJson.data.rates.CHF;
+
+  console.log(`BTC Price: ${btcPrice}`);
 });
