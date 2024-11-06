7bf8f49563512c261071f70560be24a2b08f673d
Matteo Piazza <matteo.piazza@gmail.com>
2024-11-06 14:20:57 +0100

    Update getBtcPrice.js

--- getBtcPrice.js
+++ getBtcPrice.js
@@ -1,7 +1,7 @@
 const btcRatesApiUrl = "https://api.coinbase.com/v2/exchange-rates?currency=BTC";
 
 fetch(btcRatesApiUrl).then(response => response.json()).then(data => {
-    const price = data.data.rates.CHF;
+    const price = data.data.rates.USD;
     // the console.log is required to return the value to the step
     console.log(parseFloat(price));
 });
